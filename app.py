c9be966d72ec1f05d8e72c811b91736744b98c81
M0A23219 <m0a232199c@edu.teu.ac.jp>
2025-10-25 21:49:18 +0900

    元に戻す

--- app.py
+++ app.py
@@ -105,3 +105,8 @@ def delete_all():
                 pass
     predicts.clear()
     return jsonify({'status': 'success'})
+
+# --- アプリ起動 ---
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
